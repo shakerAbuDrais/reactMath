c88ae0011ba65724c70702d0741e1a1b70f3880e
shakerAbuDrais <thecoffeiner@gmail.com>
2022-10-27 16:12:28 +0300

    Refactor using hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,22 +1,18 @@
-import React from 'react';
+import { React, useState } from 'react';
 import calculate from '../logic/calculate';
 import './Calculator.css';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { result: '0' };
-    this.obj = { total: null, next: null, operation: null };
-  }
+const Calculator = () => {
+  const [result, setResult] = useState('0');
+  const [operation, setOperation] = useState({ total: null, next: null, operation: null });
 
-clickHandler = (event) => {
-  const btnClick = event.target.innerHTML;
-  this.obj = calculate(this.obj, btnClick);
-  this.setState({ result: this.obj.next || this.obj.total || '0' });
-}
+  const clickHandler = (event) => {
+    const btnClick = event.target.innerHTML;
+    setOperation(calculate(operation, btnClick));
+    const obj = calculate(operation, btnClick);
+    setResult(obj.next || obj.total || '0');
+  };
 
-render() {
-  const { result } = this.state;
   return (
     <table className="tg" role="grid">
       <thead>
@@ -26,38 +22,37 @@ render() {
       </thead>
       <tbody>
         <tr>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-kftd">AC</td>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-kftd">+/-</td>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-kftd">%</td>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-p0tl">÷</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-kftd">AC</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-kftd">+/-</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-kftd">%</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-p0tl">÷</td>
         </tr>
         <tr>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-kftd">7</td>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-kftd">8</td>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-kftd">9</td>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-qssw">*</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-kftd">7</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-kftd">8</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-kftd">9</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-qssw">*</td>
         </tr>
         <tr>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-kftd">4</td>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-kftd">5</td>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-kftd">6</td>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-qssw">-</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-kftd">4</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-kftd">5</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-kftd">6</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-qssw">-</td>
         </tr>
         <tr>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-kftd">1</td>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-kftd">2</td>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-kftd">3</td>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-qssw">+</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-kftd">1</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-kftd">2</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-kftd">3</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-qssw">+</td>
         </tr>
         <tr>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-kftd" colSpan="2">0</td>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-kftd">.</td>
-          <td role="gridcell" onClick={this.clickHandler} className="tg-qssw">=</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-kftd" colSpan="2">0</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-kftd">.</td>
+          <td role="gridcell" onClick={clickHandler} className="tg-qssw">=</td>
         </tr>
       </tbody>
     </table>
   );
-}
-}
+};
 
 export default Calculator;
