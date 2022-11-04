2c0716edcfbec931787af5c46c67b16fd0b2ef4c
shakerAbuDrais <thecoffeiner@gmail.com>
2022-11-04 13:25:08 +0300

    Add routes

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -14,44 +14,49 @@ const Calculator = () => {
   };
 
   return (
-    <table className="tg" role="grid">
-      <thead>
-        <tr>
-          <th className="tg-xzry" colSpan="4">{result}</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td role="gridcell" onClick={clickHandler} className="tg-kftd">AC</td>
-          <td role="gridcell" onClick={clickHandler} className="tg-kftd">+/-</td>
-          <td role="gridcell" onClick={clickHandler} className="tg-kftd">%</td>
-          <td role="gridcell" onClick={clickHandler} className="tg-p0tl">÷</td>
-        </tr>
-        <tr>
-          <td role="gridcell" onClick={clickHandler} className="tg-kftd">7</td>
-          <td role="gridcell" onClick={clickHandler} className="tg-kftd">8</td>
-          <td role="gridcell" onClick={clickHandler} className="tg-kftd">9</td>
-          <td role="gridcell" onClick={clickHandler} className="tg-qssw">*</td>
-        </tr>
-        <tr>
-          <td role="gridcell" onClick={clickHandler} className="tg-kftd">4</td>
-          <td role="gridcell" onClick={clickHandler} className="tg-kftd">5</td>
-          <td role="gridcell" onClick={clickHandler} className="tg-kftd">6</td>
-          <td role="gridcell" onClick={clickHandler} className="tg-qssw">-</td>
-        </tr>
-        <tr>
-          <td role="gridcell" onClick={clickHandler} className="tg-kftd">1</td>
-          <td role="gridcell" onClick={clickHandler} className="tg-kftd">2</td>
-          <td role="gridcell" onClick={clickHandler} className="tg-kftd">3</td>
-          <td role="gridcell" onClick={clickHandler} className="tg-qssw">+</td>
-        </tr>
-        <tr>
-          <td role="gridcell" onClick={clickHandler} className="tg-kftd" colSpan="2">0</td>
-          <td role="gridcell" onClick={clickHandler} className="tg-kftd">.</td>
-          <td role="gridcell" onClick={clickHandler} className="tg-qssw">=</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="main-div">
+      <div className="left-div">
+        <h2>Let&apos;s do some math!</h2>
+      </div>
+      <table className="tg" role="grid">
+        <thead>
+          <tr>
+            <th className="tg-xzry" colSpan="4">{result}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td role="gridcell" onClick={clickHandler} className="tg-kftd">AC</td>
+            <td role="gridcell" onClick={clickHandler} className="tg-kftd">+/-</td>
+            <td role="gridcell" onClick={clickHandler} className="tg-kftd">%</td>
+            <td role="gridcell" onClick={clickHandler} className="tg-p0tl">÷</td>
+          </tr>
+          <tr>
+            <td role="gridcell" onClick={clickHandler} className="tg-kftd">7</td>
+            <td role="gridcell" onClick={clickHandler} className="tg-kftd">8</td>
+            <td role="gridcell" onClick={clickHandler} className="tg-kftd">9</td>
+            <td role="gridcell" onClick={clickHandler} className="tg-qssw">*</td>
+          </tr>
+          <tr>
+            <td role="gridcell" onClick={clickHandler} className="tg-kftd">4</td>
+            <td role="gridcell" onClick={clickHandler} className="tg-kftd">5</td>
+            <td role="gridcell" onClick={clickHandler} className="tg-kftd">6</td>
+            <td role="gridcell" onClick={clickHandler} className="tg-qssw">-</td>
+          </tr>
+          <tr>
+            <td role="gridcell" onClick={clickHandler} className="tg-kftd">1</td>
+            <td role="gridcell" onClick={clickHandler} className="tg-kftd">2</td>
+            <td role="gridcell" onClick={clickHandler} className="tg-kftd">3</td>
+            <td role="gridcell" onClick={clickHandler} className="tg-qssw">+</td>
+          </tr>
+          <tr>
+            <td role="gridcell" onClick={clickHandler} className="tg-kftd" colSpan="2">0</td>
+            <td role="gridcell" onClick={clickHandler} className="tg-kftd">.</td>
+            <td role="gridcell" onClick={clickHandler} className="tg-qssw">=</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => (
+  <>
+    <div className="home">
+      <h1> Welcome To Our Page! </h1>
+      <p>
+        Math Magicians is a website that built using React and Redux.
+        It is a simple calculator that can perform basic math operations, try it out now!
+        the whole website was built using SPA (Single Page Application) technique.
+        routes are handled by react-router-dom.
+        all the components are functional components.
+        the state is managed by redux.
+      </p>
+    </div>
+  </>
+);
+
+export default Home;
